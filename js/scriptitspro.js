a30a6b35ded206c0ff00fed6bd1cad96182e7cc3
yalatan <nikitinanf@gmail.com>
2019-09-13 01:21:57 +0300

    change css, add smoth scroll to place in page

--- js/scriptitspro.js
+++ js/scriptitspro.js
@@ -8,6 +8,17 @@
                 behavior: "smooth"
             });
         });
+        $('body').on('click', '.flowing-scroll', function() {
+            var el = $(this);
+            var dest = el.attr('href'); // получаем направление
+            if (dest !== undefined && dest !== '') { // проверяем существование
+                $('html').animate({
+                        scrollTop: $(dest).offset().top - 200 // прокручиваем страницу к требуемому элементу
+                    }, 1000 // скорость прокрутки
+                );
+            }
+            return false;
+        });
     });
 })(jQuery);
 
