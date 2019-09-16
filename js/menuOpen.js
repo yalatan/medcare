99e6329c6a314bc7141d9c1915e42127038a20ab
yalatan <nikitinanf@gmail.com>
2019-09-16 20:20:42 +0300

    change style

--- js/menuOpen.js
+++ js/menuOpen.js
@@ -1,53 +1,68 @@
 (function($) {
     $(document).ready(function() {
-        console.log("rtyui");
 
         function menu_close() {
             $("#menu_button_open").fadeIn(800).css("display", "flex");
-            $("#menu_button_close").fadeOut(800);
-            $("#menu_open_wrapper").fadeOut(800);
+            $("#menu_button_close").fadeOut(500);
+            $("#menu_open_wrapper").fadeOut(500);
+
         };
 
         function menu_open() {
-            console.log($(".header_wrapper").height());
             $("#menu_button_close").fadeIn(800).css("display", "flex");
-            $("#menu_open_wrapper").fadeIn(800).offset({ top: $(".header_wrapper").height() + $(window).scrollTop() });
-            $("#menu_button_open").fadeOut(800);
+            $("#menu_open_wrapper").fadeIn(800).offset({ top: $(".header_wrapper").height() - 1 });
+            $("#menu_button_open").fadeOut(500);
+
+
         };
 
         function search_close() {
             $("#search_button_open").fadeIn(800).css("display", "flex");
-            $("#search_button_close").fadeOut(800);
-            $("#search_open_wrapper").fadeOut(800);
+            $("#search_button_close").fadeOut(500);
+            $("#search_open_wrapper").fadeOut(500);
+
         };
 
         function search_open() {
             $("#search_button_close").fadeIn(800).css("display", "flex");
-            $("#search_open_wrapper").fadeIn(800).offset({ top: $(".header_wrapper").height() + $(window).scrollTop() });
+            $("#search_open_wrapper").fadeIn(800).offset({ top: $(".header_wrapper").height() - 1 });
             $("#search_open_wrapper").fadeIn(800);
-            $("#search_button_open").fadeOut(800);
+            $("#search_button_open").fadeOut(500);
+
         };
 
+        function scrollTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        };
 
 
         /**menu open */
         $('body').on('click', '#menu_button_open ', function() {
+            scrollTop();
             menu_open();
             search_close();
+            $(".black_color").fadeIn(100);
         });
         /*menu close*/
         $('body').on('click', '#menu_button_close', function() {
             menu_close();
+            $(".black_color").fadeOut(100);
         });
 
         /**search open */
         $('body').on('click', '#search_button_open', function() {
+            scrollTop();
             search_open();
             menu_close();
+            $(".black_color").fadeIn(100);
         });
         /*search close*/
         $('body').on('click', '#search_button_close', function() {
             search_close();
+            $(".black_color").fadeOut(100);
         });
 
 
