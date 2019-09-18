4258888bbaf8399d7a23f84e77b864f899e01c7a
yalatan <nikitinanf@gmail.com>
2019-09-18 12:06:50 +0300

    change style

--- js/navigationScroll.js
+++ js/navigationScroll.js
@@ -5,7 +5,7 @@
             if ($(window).width() > 758) {
                 if ($(window).scrollTop() == 0) {
                     $("body .containerwidth100vw").css({ 'backgroundColor': 'transparent;' });
-                    $("body .header .header_logo").css({ 'padding': '0' }).height("auto").css("marginTop", 20);
+                    $("body .header .header_logo").height("auto").css("marginTop", 0);
                     $("body .header_navigation .wrap_contacts").show();
                     $("#menu_open_wrapper").offset({ top: $(".header_wrapper").height() + $(window).scrollTop() });
                     $("#search_open_wrapper").offset({ top: $(".header_wrapper").height() + $(window).scrollTop() });
@@ -14,7 +14,7 @@
                     $("body .containerwidth100vw").css({ 'backgroundColor': '#ffffff' }).height(60);
                     $("#menu_open_wrapper").offset({ top: $(".header_wrapper").height() + $(window).scrollTop() });
                     $("#search_open_wrapper").offset({ top: $(".header_wrapper").height() + $(window).scrollTop() });
-                    $("body .header .header_logo").css("marginTop", 0).height(50);
+                    $("body .header .header_logo").height(50);
                     $("body .header_navigation .wrap_contacts").hide();
 
                     $("body .menu .button_blue").show();
